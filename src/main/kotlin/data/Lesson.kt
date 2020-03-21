a0c0fba8017cff626c7ddf2059b167c39d4af3ae
Ricyko <31647766+Ricyko@users.noreply.github.com>
2020-03-21 19:16:22 +0600

    Add files via upload

---
+++ src/main/kotlin/data/Lesson.kt
@@ -0,0 +1,14 @@
+package data
+
+
+
+data class Lesson(
+    val name: String
+)
+
+val Predmet
+        = arrayListOf(
+    Lesson("Philosophy"),
+    Lesson("Information Processing Theories"),
+    Lesson("OOP")
+)
