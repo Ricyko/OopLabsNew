d2ebf3c354b73273434a41c6b4576a74776fd15d
Ricyko <31647766+Ricyko@users.noreply.github.com>
2020-04-12 16:26:04 +0600

    Add files via upload

---
+++ src/main/kotlin/component/AddLesson.kt
@@ -0,0 +1,64 @@
+package component
+
+import data.Lesson
+import data.Student
+import kotlinx.html.InputType
+import kotlinx.html.id
+import kotlinx.html.js.onChangeFunction
+import kotlinx.html.js.onClickFunction
+import kotlinx.html.onChange
+import org.w3c.dom.HTMLInputElement
+import org.w3c.dom.events.Event
+import react.*
+import react.dom.*
+import kotlin.browser.document
+
+interface Props : RProps {
+   /* abstract val onClick: */
+    var lessons: Array<Lesson>
+    var new: (String) -> (Event) -> Unit
+}
+
+interface State : RState {
+    var newLesson: String
+}
+
+class newaddLesson : RComponent<Props, State>() {
+    override fun componentWillMount() {
+        state.newLesson = ""
+    }
+
+    override fun RBuilder.render() {
+        h2 {
+            +"Add new lesson"
+        }
+
+        div {
+            input(type = InputType.text) {
+                attrs {
+                    value = state.newLesson
+                    onChangeFunction = {
+                        val target = it.target as HTMLInputElement
+                        setState {
+                            newLesson = target.value
+                        }
+
+                    }
+                }
+            }
+            button {
+                +"plus"
+                attrs.onClickFunction = props.new(state.newLesson)
+            }
+
+        }
+    }
+}
+
+fun RBuilder.addlesson(
+    lessons: Array<Lesson>,
+    new: (String) -> (Event) -> Unit
+) = child(newaddLesson::class) {
+    attrs.lessons = lessons
+    attrs.new = new
+}
