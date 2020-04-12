3e374d2acf31323e536b141dab9c39c2e0a8a3cc
Ricyko <31647766+Ricyko@users.noreply.github.com>
2020-04-12 18:03:33 +0600

    Update AddLesson.kt

--- src/main/kotlin/component/AddLesson.kt
+++ src/main/kotlin/component/AddLesson.kt
@@ -1,64 +1,80 @@
 package component
-
-import data.Lesson
-import data.Student
-import kotlinx.html.InputType
-import kotlinx.html.id
-import kotlinx.html.js.onChangeFunction
-import kotlinx.html.js.onClickFunction
-import kotlinx.html.onChange
-import org.w3c.dom.HTMLInputElement
+import data.*
 import org.w3c.dom.events.Event
 import react.*
-import react.dom.*
-import kotlin.browser.document
+import react.dom.h1
+import react.dom.h2
 
-interface Props : RProps {
-   /* abstract val onClick: */
+interface AppProps : RProps {
+    var students: Array<Student> }
+interface AppState : RState {
+    var presents: Array<Array<Boolean>>             //CHANGED
     var lessons: Array<Lesson>
-    var new: (String) -> (Event) -> Unit
 }
 
-interface State : RState {
-    var newLesson: String
-}
-
-class newaddLesson : RComponent<Props, State>() {
+class New : RComponent<AppProps, AppState>() {
     override fun componentWillMount() {
-        state.newLesson = ""
+        state.lessons = lessonsList                     //changed
+        state.presents = Array(state.lessons.size) {
+            Array(props.students.size) { false }
+        }
+    }
+    override fun RBuilder.render() {
+        h1 { +"Lesson" }
+        h2{+"Add new lesson"}
+        LessonAdd(plus())
+        lessonListFull(
+            state.lessons,
+            props.students,
+            state.presents,
+            onClickLessonFull
+        )
+        studentListFull(
+            state.lessons,
+            props.students,
+            transform(state.presents),
+            onClickStudentFull
+        )
     }
 
-    override fun RBuilder.render() {
-        h2 {
-            +"Add new lesson"
+    fun transform(source: Array<Array<Boolean>>) =
+        Array(source[0].size){row->
+            Array(source.size){col ->
+                source[col][row]
+            }
         }
 
-        div {
-            input(type = InputType.text) {
-                attrs {
-                    value = state.newLesson
-                    onChangeFunction = {
-                        val target = it.target as HTMLInputElement
-                        setState {
-                            newLesson = target.value
-                        }
-
-                    }
-                }
+    fun onClick(indexLesson: Int, indexStudent: Int) =
+        { _: Event ->
+            setState {
+                presents[indexLesson][indexStudent] =
+                    !presents[indexLesson][indexStudent]
             }
-            button {
-                +"plus"
-                attrs.onClickFunction = props.new(state.newLesson)
-            }
+        }
 
+    val onClickLessonFull =
+        { indexLesson: Int ->
+            { indexStudent: Int ->
+                onClick(indexLesson, indexStudent)
+            }
+        }
+
+    val onClickStudentFull =
+        { indexStudent: Int ->
+            { indexLesson: Int ->
+                onClick(indexLesson, indexStudent)
+            }
+        }
+
+    private fun plus():(String) -> Unit = { str->
+        setState {
+            lessons += Lesson(str)
+            presents += arrayOf(Array(props.students.size){false})
         }
     }
 }
-
-fun RBuilder.addlesson(
-    lessons: Array<Lesson>,
-    new: (String) -> (Event) -> Unit
-) = child(newaddLesson::class) {
-    attrs.lessons = lessons
-    attrs.new = new
+fun RBuilder.app(
+    students: Array<Student>
+) = child(New::class) {
+    attrs.students = students
 }
