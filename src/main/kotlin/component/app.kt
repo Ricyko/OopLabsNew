5e7b06a0bb4ec32a28d425212c24cedb6a4ac1f3
Ricyko <31647766+Ricyko@users.noreply.github.com>
2020-05-05 15:55:54 +0600

    Update app.kt

--- src/main/kotlin/component/app.kt
+++ src/main/kotlin/component/app.kt
@@ -13,7 +13,8 @@ interface AppState : RState {
     var students: Array<Student>
     var presents: Array<Array<Boolean>> }
 interface Route : RProps {
-    var number: String }
+    var number: String
+}
 class App : RComponent<AppProps, AppState>() {
     override fun componentWillMount() {
         state.less = lessonsList
@@ -28,18 +29,20 @@ class App : RComponent<AppProps, AppState>() {
                 ul {
                     li { navLink("/lessons") { +"Lessons" } }
                     li { navLink("/students") { +"Students" } }
-                    li {navLink("/editStudents"){+"Redact Students"} }
-                    li {navLink("/editLessons"){+"Redact Lessons"} }
+                    li { navLink("/editStudents") { +"Redact Students" } }
+                    li { navLink("/editLessons") { +"Redact Lessons" } }
                 } } }
         switch {
             route("/lessons",
                 exact = true,
                 render = {
-                    anyList(state.less, "Lessons", "/lessons") })
+                    anyList(state.less, "Lessons", "/lessons", deleteforlesson())
+                })
             route("/students",
                 exact = true,
                 render = {
-                    anyList(state.students, "Students", "/students") })
+                    anyList(state.students, "Students", "/students", deleteforstudent())
+                })
             route("/lessons/:number",
                 render = { route_props: RouteResultProps<Route> ->
                     val number = route_props.match.params.number.toIntOrNull() ?: -1
@@ -64,70 +67,83 @@ class App : RComponent<AppProps, AppState>() {
                             }.toTypedArray()
                         ) { onClick(it, number) }
                     else
-                        p { +"No student here" }
-                })
+                        p { +"No student here" } })
             route("/editStudents",
                 render = {
+                    console.log(state.less)
+                    console.log(state.presents)
+                    console.log(state.students)
                     Redact(
-                        RBuilder::anyList, RBuilder::StudentAdd, state.students, addforstudent(), deleteforstudent(),
+                        RBuilder::anyList,
+                        RBuilder::StudentAdd,
+                        state.students,
+                        addforstudent(),
+                        deleteforstudent(),
                         "Students",
                         "/students"
-                    ) })
+                    )
+                })
             route("/editLessons",
                 render = {
                     Redact(
-                        RBuilder::anyList, RBuilder::LessonAdd, state.less, addforelesson(), deleteforlesson(),
+                        RBuilder::anyList,
+                        RBuilder::LessonAdd,
+                        state.less,
+                        addforlesson(),
+                        deleteforlesson(),
                         "Lessons",
-                        "/lessons")
-                })
-        } }
-    fun addforelesson():(Event) -> Unit = {
-        val objectnew = document.getElementById("Lessons") as HTMLInputElement
-        setState {
-            less += Lesson(objectnew.value)
-            presents += arrayOf(Array(state.students.size){false})
-        } }
-    fun deleteforlesson() :(Event) -> Unit = {
-        val objectnew = document.getElementById("DeleteLessons") as HTMLInputElement
-        val redactLesson = state.less.toMutableList().apply {
-            removeAt(objectnew.value.toInt()-1) }
-            .toTypedArray()
-        val redactPresent = state.presents.toMutableList().apply {
-            removeAt(objectnew.value.toInt()-1) }
-            .toTypedArray()
-        setState{
-            less = redactLesson
-            presents=redactPresent
-        } }
-    fun addforstudent():(Event) -> Unit = {
-        val objectnew = document.getElementById("Students") as HTMLInputElement
-        val newvalue = objectnew.value.split(" ")
-        setState {
-            students += Student(newvalue[0],newvalue[1])
-            presents += arrayOf(Array(state.students.size){false})
-        } }
-    fun deleteforstudent() :(Event) -> Unit = {
-        val objectnew = document.getElementById("DeleteStudents") as HTMLInputElement
-        val redactStudent = state.students.toMutableList().apply {
-            removeAt(objectnew.value.toInt()-1) }
-            .toTypedArray()
-        val redactPresent = state.presents.toMutableList().apply {
-            removeAt(objectnew.value.toInt()-1) }
-            .toTypedArray()
-        setState{
-            students = redactStudent
-            presents= redactPresent
-        } }
+                        "/lessons"
+                    )
+                }) } }
     fun onClick(indexLesson: Int, indexStudent: Int) =
         { _: Event ->
             setState {
                 presents[indexLesson][indexStudent] =
-                    !presents[indexLesson][indexStudent] } } }
-fun RBuilder.app() =
-    child(
-        withDisplayName("AppHoc", App::class)
-    ) {
+                    !presents[indexLesson][indexStudent]
+            } }
+    fun addforlesson    ():(Event) -> Unit = {
+        val objectnew = document.getElementById("Lessons") as HTMLInputElement
+        val redactLess = state.presents.mapIndexed { index, _ ->
+            state.presents[index].plus(arrayOf(false))
+        }.toTypedArray()
+        setState {
+            less += Lesson(objectnew.value)
+            presents = redactLess } }
+    fun deleteforlesson() :(Int) -> Unit = {
+        val redactLess = state.less.toMutableList().apply {
+            removeAt(it) }
+            .toTypedArray()
+        val editedPresents = state.presents.mapIndexed { index, _ ->
+            state.presents[index].toMutableList().apply {
+                removeAt(it)
+            }.toTypedArray()
+        }.toTypedArray()
+        setState{
+            less = redactLess
+            presents= editedPresents } }
+    fun addforstudent():(Event) -> Unit = {
+        val objectnew = document.getElementById("Students") as HTMLInputElement
+        val value = objectnew.value.split(" ")
+        setState {
+            students += Student(value[0],value[1])
+            presents += arrayOf(Array(state.students.size){false}) } }
+    fun deleteforstudent() :(Int) -> Unit = {
+        val redactStud = state.students.toMutableList().apply {
+            removeAt(it) }
+            .toTypedArray()
+        val editedPresents = state.presents.toMutableList().apply {
+            removeAt(it)}
+            .toTypedArray()
+        setState{
+            students = redactStud
+            presents= editedPresents
+        }
     }
-
+}
+    fun RBuilder.app() =
+        child(
+            withDisplayName("AppHoc", App::class)
+        ) {
+        }
 
 
