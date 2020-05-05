ceb618925ac6c04c5a867916c6a91d01cfd0f370
Ricyko <31647766+Ricyko@users.noreply.github.com>
2020-05-05 15:57:15 +0600

    Update ObshiyComponent.kt

--- src/main/kotlin/component/ObshiyComponent.kt
+++ src/main/kotlin/component/ObshiyComponent.kt
@@ -5,14 +5,14 @@ import react.*
 import react.dom.*
 interface Props3<O> :RProps{
 /*    abstract var delete: (Event) -> Unit*/
-   var deletenew : (Event) -> Unit
+    var deletenew : (Int) -> Unit
     var dobavlenie : (Event) -> Unit
     var way : String
     var name : String
     var subject : Array<O>}
 fun <O> Redact(
     rAddObj :RBuilder.() -> ReactElement,
-    Component: RBuilder.(Array<O>, String, String)->ReactElement
+    Component: RBuilder.(Array<O>, String, String,(Int) -> Unit)->ReactElement
 ) = functionalComponent<Props3<O>> {props ->
         h1{+"you can add or delete students/lessons"}
         h2{+"write something"}
@@ -22,22 +22,23 @@ fun <O> Redact(
             br {}
             button {
                 +"ADD"
-                attrs.onClickFunction = props.dobavlenie }
-            button {
-                +"DEL"
-                attrs.onClickFunction = props.deletenew }
-            Component(props.subject,props.name,props.way) } }
+                attrs.onClickFunction = props.dobavlenie
+
+        }
+            Component(props.subject,props.name,props.way,props.deletenew)
+        }
+}
 fun <O> RBuilder.Redact(
-    Component: RBuilder.(Array<O>, String, String)->ReactElement,
+    rComponent: RBuilder.(Array<O>, String, String,(Int) -> Unit)->ReactElement,
     addobject :RBuilder.() -> ReactElement,
     subject : Array<O>,
     dobavlenie: (Event) -> Unit,
-    deletenew: (Event) -> Unit,
+    deletenew: (Int) -> Unit,
     name : String,
     way: String
-) = child(Redact<O>(addobject,Component)){
+) =  child(Redact<O>(addobject,rComponent)){
     attrs.dobavlenie=dobavlenie
     attrs.deletenew=deletenew
     attrs.subject= subject
     attrs.name = name
-    attrs.way = way
+    attrs.way = way}
